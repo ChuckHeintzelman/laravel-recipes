b808e17529871047a98d6cabbd746f271f38991f
Chuck Heintzelman <chuckh@gmail.com>
2014-09-27 15:23:23 +0000

    config twiddling

--- app/config/database.php
+++ app/config/database.php
@@ -15,7 +15,7 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => $_ENV['DB_HOST'],
-            'database'  => $_ENV['DB_NAME'],
+            'database'  => 'recipes',
             'username'  => $_ENV['DB_USERNAME'],
             'password'  => $_ENV['DB_PASSWORD'],
             'charset'   => 'utf8',
--- app/config/local/database.php
+++ app/config/local/database.php
@@ -3,7 +3,6 @@ return [
 	'connections' => [
 		'mysql' => [
 			'host'     => 'localhost',
-			'name'     => 'recipes',
 			'username' => 'homestead',
 			'password' => 'secret',
 		],
